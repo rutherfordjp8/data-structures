c30aacb0aa1524898052c5d788d79f204c4db957
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:24:20 -0700

    Completed size

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+};
+
+
+Queue.prototype.enqueue = function() {
+  
+};
+
+Queue.prototype.dequeue = function() {
+  
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;  
 };
 
 
