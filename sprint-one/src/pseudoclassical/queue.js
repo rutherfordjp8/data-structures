a3cc233bc8aa7106bb328edcad62fb427c0d3009
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:33:15 -0700

    Implement queue full pass, started stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -5,12 +5,24 @@ var Queue = function() {
 };
 
 
-Queue.prototype.enqueue = function() {
-  
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.size()] = value;
 };
 
 Queue.prototype.dequeue = function() {
+  var temp = this.storage['0'];
+  delete this.storage['0'];
   
+  if (this.size() > 0) {
+    var index = 0;
+    for (var key in this.storage) {
+      this.storage[index] = this.storage[index + 1];
+      delete this.storage[index + 1];
+      index++;
+    }
+  }
+
+  return temp;
 };
 
 Queue.prototype.size = function() {
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Object.prototype.push = function() {
+
+};
+
+Object.prototype.pop = function() {
+
+};
+
+Object.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
