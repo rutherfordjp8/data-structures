5fd4da68f058eed31d373bf2f1846a8f76da3a9b
Hack Reactor Student <student@hackreactor.com>
2017-07-05 19:33:28 -0700

    Reimplement functional shared using extend

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,12 +1,8 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {};
-
+  var someInstance = _.extend({}, queueMethods);
   someInstance.storage = {};
-  someInstance.enqueue = queueMethods.enqueue;
-  someInstance.dequeue = queueMethods.dequeue;
-  someInstance.size = queueMethods.size;
 
   return someInstance;
 };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {};
+  var someInstance = _.extend({}, stackMethods);
 
   someInstance.storage = {};
-  someInstance.push = stackMethods.push;
-  someInstance.pop = stackMethods.pop;
-  someInstance.size = stackMethods.size;
+  
   return someInstance;
 };
 
