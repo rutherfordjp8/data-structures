b916793f3afedf18696a1fec792b89e96beffab2
Hack Reactor Student <student@hackreactor.com>
2017-07-07 10:50:44 -0700

    Reimplement Graph object and implement addNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,12 +2,12 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.storage = {};
-  this.numberOfKeys = Object.keys(this.storage).length;
+  this.storage = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
