34e02a999b2b4fe170a62a26373c16fe73f54649
Hack Reactor Student <student@hackreactor.com>
2017-07-07 11:22:57 -0700

    Implement removeEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,23 +2,23 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.storage = [];
+  this.nodes = [];
   this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.storage.push(node);
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  if (this.storage.length === 0) {
+  if (this.nodes.length === 0) {
     return false;
   }
 
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i] === node) {
+  for (var i = 0; i < this.nodes.length; i++) {
+    if (this.nodes[i] === node) {
       return true;
     }
   }
@@ -28,7 +28,7 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  if (this.storage.length === 0) {
+  if (this.nodes.length === 0) {
     return -1;
   }
 };
@@ -60,6 +60,17 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if(this.edges.length === 0) {
+    return -1;
+  }
+
+  for (var i = 0; i < this.edges.length; i++) {
+    if ((this.edges[i][0] === fromNode && this.edges[i][1] === toNode) 
+        || (this.edges[i][0] === toNode && this.edges[i][1] === fromNode)) {
+      this.edges = this.edges.slice(0,i).concat(this.edges.slice(i+1));
+    }
+//  edges.slice(0,i) edges.slice(i+1)
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
