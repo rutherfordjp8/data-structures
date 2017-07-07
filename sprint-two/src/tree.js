6e1dd3592f264b1846e49b9d1262ebcad5418d6a
Hack Reactor Student <student@hackreactor.com>
2017-07-06 21:01:01 -0700

    Answered Time Complexity question for Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -40,5 +40,7 @@ treeMethods.contains = function(target) {
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? 
+ * addChild - Constant, O(1)
+ * contains - Linear, O(n)
  */
