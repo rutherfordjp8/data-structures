f5b3e96d59ba23ae6787e284dd5004111a20bbe8
Hack Reactor Student <student@hackreactor.com>
2017-07-07 15:34:45 -0700

    Implement hashtable insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,6 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //create an array "pairs" for the posistion of the index in this.storage.
+  //push key value pairs into the array of pairs.
+
+  //if an array of pairs does not exist at index...
+  if (!Array.isArray(this._storage.get(index))) {
+    //create an array of pairs
+    var pairs = [];
+    //create an array for key value pairs 
+    //then push key value pairs into "pairs".
+    pairs.push([k, v]);
+    //insert this new array into the index
+    this._storage.set(index, pairs);
+  } else {
+    //if array "pairs" already exist
+    //if key exists in pairs
+    var ind = containsKey(this._storage.get(index), k);
+    if (ind !== -1) {
+      //override the value in the key/value pair in array "pairs"
+      this._storage.get(index)[ind][1] = v;
+    } else {
+      //else, push key/value pair to 'pairs'
+      this._storage.get(index).push([k, v]);    
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -17,7 +41,19 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
-
+var containsKey = function(collection, value) {
+  if (collection.length === 0) {
+    return -1;
+  }
+  
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i][0] === value) {
+      return i;
+    }
+  }
+  
+  return -1;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -15,14 +15,17 @@ var LimitedArray = function(limit) {
   var storage = [];
 
   var limitedArray = {};
+
   limitedArray.get = function(index) {
     checkLimit(index);
     return storage[index];
   };
+
   limitedArray.set = function(index, value) {
     checkLimit(index);
     storage[index] = value;
   };
+
   limitedArray.each = function(callback) {
     for (var i = 0; i < storage.length; i++) {
       callback(storage[i], i, storage);
