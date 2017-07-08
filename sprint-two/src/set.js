029981271f9559ba72a877dd9c9676879c16cf7a
Hack Reactor Student <student@hackreactor.com>
2017-07-08 09:59:40 -0700

    Refactor set implementation to use hashes (Objects); time comp is constant

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,44 +1,48 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set.storage = [];
+  // set.storage = [];
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (!this.contains(item)) {
-    this.storage.push(item);
-  } else {
-    console.log(item, 'Already exists in the set!');
-  }
+  // if (!this.contains(item)) {
+  //   this.storage.push(item);
+  // } else {
+  //   console.log(item, 'Already exists in the set!');
+  // }
+  this.storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
-  if (this.storage.length === 0) {
-    return false;  
-  }
-    
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i] === item) {
-      return true;
-    }
-  }
-  
-  return false;
+  // if (this.storage.length === 0) {
+  //   return false;
+  // }
+  //
+  // for (var i = 0; i < this.storage.length; i++) {
+  //   if (this.storage[i] === item) {
+  //     return true;
+  //   }
+  // }
+
+  // return false;
+  return this.storage[item];
 };
 
 setPrototype.remove = function(item) {
-  if (this.storage.length === 0) {
-    return undefined;
-  }
-  
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i] === item) {
-      this.storage = this.storage.slice(0, i).concat(this.storage.slice(i + 1));
-      break;
-    }
-  }
+  // if (this.storage.length === 0) {
+  //   return undefined;
+  // }
+  //
+  // for (var i = 0; i < this.storage.length; i++) {
+  //   if (this.storage[i] === item) {
+  //     this.storage = this.storage.slice(0, i).concat(this.storage.slice(i + 1));
+  //     break;
+  //   }
+  // }
+  this.storage[item] = false;
 };
 
 /*
@@ -46,4 +50,13 @@ setPrototype.remove = function(item) {
  * add - O(1)
  * contains - O(n)
  * remove - O(n)
+ *
+ * After refactoring the code
+ * add - O(1)
+ * contains - O(1)
+ * remove - O(1)
+ *
+ * Many thanks to Brian The Senior who explained the notion of using hashes
+ * (objects) instead of arrays and application of them to this particular
+ * problem.
  */
