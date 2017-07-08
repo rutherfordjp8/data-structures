23f3df2b3d8e109a019f103c575a567fb47bbc81
Hack Reactor Student <student@hackreactor.com>
2017-07-07 19:52:02 -0700

    Implement insert tests pass

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,38 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(BinarySearchTreeMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
 
+var BinarySearchTreeMethods = {};
 
+BinarySearchTreeMethods.insert = function(value) {
+  var node = new BinarySearchTree(value);
+  
+  //if (value >= this.value)
+  if (value >= this.value) {
+    //if this.right is null
+    if (this.right === null) {
+      //this.right = node
+      this.right = node;
+    } else {
+      //else this.right.insert(value);
+      this.right.insert(value);
+    }
+  } else {
+  //else
+    //if this.left is null
+    if (this.left === null) {
+      //this.left = node
+      this.left = node;
+    } else {
+      //else this.left.insert(value);
+      this.left.insert(value);
+    }
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
